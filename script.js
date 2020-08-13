1e7f2130ff4536e511a8582ee4701cbcd1c30691
Zach Bryan <zacharybryan@icloud.com>
2020-08-13 14:31:46 -0700

    js edits, added seach bar

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // API KEY
-const apiKey = a2e2e87c947af1ae1888811705b0441c;
+// const apiKey = a2e2e87c947af1ae1888811705b0441c;
 
-const currentDate = (mm/dd/yyyy);
+// const currentDate = (mm/dd/yyyy);
 
-const location = "San Diego"
+const inputLocation = "San Diego"
 
-const forecastURL = "api.openweathermap.org/data/2.5/forecast?q=" + "SanDiego" + "&appid=" + apiKey;
 
-console.log(forecastURL);
+
+const forecastURL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=a2e2e87c947af1ae1888811705b0441c&q=" + inputLocation;
+
+
+$.ajax({
+    url: forecastURL,
+    method: "GET"
+
+})
+.then(function (weather){
+    console.log(forecastURL);
+})
+
+
