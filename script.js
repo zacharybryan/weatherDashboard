84b2358aadcba197931b44f1855178e0325fa6ec
Zach Bryan <zacharybryan@icloud.com>
2020-08-13 13:16:32 -0700

    linked moment and bootstrap js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const apiKey = a2e2e87c947af1ae1888811705b0441c;
 
 const currentDate = (mm/dd/yyyy);
 
+const location = "San Diego"
+
+const forecastURL = "api.openweathermap.org/data/2.5/forecast?q=" + "SanDiego" + "&appid=" + apiKey;
+
+console.log(forecastURL);
