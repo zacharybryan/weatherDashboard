f43d590377eed2f0b577003d240dcf34ac713c4d
Zach Bryan <zacharybryan@icloud.com>
2020-08-13 17:27:01 -0700

    button work

--- script.js
+++ script.js
@@ -7,7 +7,19 @@
 
 $(document).ready(function(){
 
+    const getWeather = function (getWeather){
 
+        const forecastURL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=a2e2e87c947af1ae1888811705b0441c&q=" + inputLocation;
+    
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        
+        })
+        .then(function (weather){
+            console.log(forecastURL);
+        })
+    };
 
 $("#searchButton").on("click", function (getValue){
     const inputLocation = $("#citySearch").val();
@@ -18,21 +30,9 @@ $("#searchButton").on("click", function (getValue){
     }
 );
 
-const getWeather = function (getWeather){
 
 
-}
 
-const forecastURL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=a2e2e87c947af1ae1888811705b0441c&q=" + inputLocation;
-
-$.ajax({
-    url: forecastURL,
-    method: "GET"
-
-})
-.then(function (weather){
-    console.log(forecastURL);
-})
 })
 
 
